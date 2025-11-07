7a97f5884b0d4af8c1499eeeb7028a4ad1b83711
Aman Kumar Singh <er.amansingh4699@gmail.com>
2025-11-08 00:18:56 +0530

    templetes added

--- employee_dashboard/employee_dashboard/settings.py
+++ employee_dashboard/employee_dashboard/settings.py
@@ -73,13 +73,17 @@ WSGI_APPLICATION = 'employee_dashboard.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'knacklink_db',
+        'USER': 'knacklink_user',
+        'PASSWORD': 'Knacklink@12345',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
