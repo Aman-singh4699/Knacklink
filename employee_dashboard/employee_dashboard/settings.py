d69f6cea5ede41ba993ffa17fac2fca411bce798
Aman-singh4699 <er.amansingh4699@gmail.com>
2025-11-11 21:34:15 +0530

    Fix DATABASE_URL fallback for Railway

--- employee_dashboard/employee_dashboard/settings.py
+++ employee_dashboard/employee_dashboard/settings.py
@@ -57,14 +57,13 @@ TEMPLATES = [
 WSGI_APPLICATION = "employee_dashboard.wsgi.application"
 
 # DATABASE (auto detects Railway DATABASE_URL, else falls back to local)
+
 DATABASES = {
     "default": dj_database_url.config(
-        default=f"postgres://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}",
+        default=os.environ.get("DATABASE_URL", "sqlite:///db.sqlite3"),
         conn_max_age=600,
-        ssl_require=False if DEBUG else True,
     )
 }
-
 # PASSWORD VALIDATION
 AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
